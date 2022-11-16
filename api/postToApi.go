cc61e926589e03d799e141cf93b4dc270c48c509
Seyit Ahmet Gökçe <gokce9158@gmail.com>
2022-11-16 22:12:47 +0300

    Update postToApi.go

--- api/postToApi.go
+++ api/postToApi.go
@@ -14,6 +14,7 @@ import (
 
 const (
 	serviceURL = "https://source.trademarketdata.com/api/paper-news/store-bulk"
+	limit      = 500
 )
 
 // PostToApi sends the news to the API
@@ -34,45 +35,82 @@ func PostToApi(newsList model.NewsClNewsList) error {
 	if len(newsListToSend.News) == 0 {
 		log.Println("No news to send to the API.")
 	} else {
-		jsonBody, err := json.Marshal(newsListToSend)
-		if err != nil {
-			return err
-		}
-		bodyReader := bytes.NewReader(jsonBody)
-
-		req, err := http.NewRequest(http.MethodPost, serviceURL, bodyReader)
-
-		if err != nil {
-			return err
-			//fmt.Printf("client: could not create request: %s\n", err)
-			//os.Exit(1)
-		}
-		req.Header.Set("Content-Type", "application/json")
-
-		client := http.Client{
-			Timeout: 30 * time.Second,
-		}
-
-		res, err1 := client.Do(req)
-		if err1 != nil {
-			return err1
-			//fmt.Printf("client: error making http request: %s\n", err)
-			//os.Exit(1)
-		}
-
-		defer res.Body.Close()
-
-		if res.StatusCode != http.StatusOK {
-			bodyBytes, _ := io.ReadAll(res.Body)
-			fmt.Println(string(bodyBytes))
-			return fmt.Errorf("error sending news to the API. Status code: %d", res.StatusCode)
-		}
-
-		for _, news := range newsList.News {
-			err = repository.Mongo.FlagSucceeded(news)
+		if len(newsListToSend.News) > limit {
+			log.Printf("Sending %d news to the API", limit)
+			newsLists := splitList(newsListToSend)
+			for _, newsList := range newsLists {
+				err := postToApi(newsList)
+				if err != nil {
+					return err
+				}
+			}
+		} else {
+			log.Printf("Sending %d news to the API", len(newsListToSend.News))
+			err := postToApi(newsListToSend)
 			if err != nil {
 				return err
 			}
+		}
+
+	}
+	return nil
+}
+
+func splitList(newsList model.NewsClNewsList) []model.NewsClNewsList {
+	var newsLists []model.NewsClNewsList
+
+	for i := 0; i < len(newsList.News); i += limit {
+		end := i + limit
+
+		if end > len(newsList.News) {
+			end = len(newsList.News)
+		}
+
+		newsLists = append(newsLists, model.NewsClNewsList{News: newsList.News[i:end]})
+	}
+
+	return newsLists
+}
+
+func postToApi(newsList model.NewsClNewsList) error {
+	jsonBody, err := json.Marshal(newsList)
+	if err != nil {
+		return err
+	}
+	bodyReader := bytes.NewReader(jsonBody)
+
+	req, err := http.NewRequest(http.MethodPost, serviceURL, bodyReader)
+
+	if err != nil {
+		return err
+		//fmt.Printf("client: could not create request: %s\n", err)
+		//os.Exit(1)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := http.Client{
+		Timeout: 30 * time.Second,
+	}
+
+	res, err1 := client.Do(req)
+	if err1 != nil {
+		return err1
+		//fmt.Printf("client: error making http request: %s\n", err)
+		//os.Exit(1)
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		bodyBytes, _ := io.ReadAll(res.Body)
+		fmt.Println(string(bodyBytes))
+		return fmt.Errorf("error sending news to the API. Status code: %d", res.StatusCode)
+	}
+
+	for _, news := range newsList.News {
+		err = repository.Mongo.FlagSucceeded(news)
+		if err != nil {
+			return err
 		}
 	}
 
