688aff5ee8622158cf29cd8ba45e5c0f7ceeb425
Seyit Ahmet Gökçe <gokce9158@gmail.com>
2022-11-18 14:23:53 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,31 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"newscl/api"
 	"newscl/model"
 	"newscl/repository"
-)
+	"time"
 
-const (
-	port = 9999
+	sch "github.com/gbenroscience/scheduled-executor/utils"
 )
 
 func main() {
 
 	repository.InitMongoDB()
 
-	log.Println("Starting the task")
+	log.Println("Starting the scheduled task")
 
-	bbcNews, _ := repository.GetNewsByProvider(model.BBC)
-	errBBC := api.PostToApi(bbcNews, model.BBC)
-	if errBBC != nil {
-		log.Fatalf("Error posting BBC news to API: %v", errBBC)
-	}
+	executor := sch.NewTimedExecutor(time.Second*3, time.Hour)
 
-	reutersNews, _ := repository.GetNewsByProvider(model.REUTERS)
-	errReuters := api.PostToApi(reutersNews, model.REUTERS)
-	if errReuters != nil {
-		log.Fatalf("Error posting Reuters news to API: %v", errReuters)
-	}
+	executor.Start(func() {
+		bbcNews, _ := repository.GetNewsByProvider(model.BBC)
+		errBBC := api.PostToApi(bbcNews, model.BBC)
+		if errBBC != nil {
+			log.Fatalf("Error posting BBC news to API: %v", errBBC)
+		}
+
+	}, true)
+
+	executor.Start(func() {
+		reutersNews, _ := repository.GetNewsByProvider(model.REUTERS)
+		errReuters := api.PostToApi(reutersNews, model.REUTERS)
+		if errReuters != nil {
+			log.Fatalf("Error posting Reuters news to API: %v", errReuters)
+		}
+	}, true)
+
+	fmt.Scanln()
 }
